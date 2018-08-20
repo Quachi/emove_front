0f337832ed6e094842f54842db40d14e5f5bd19d
Quach <kevin.quach@outlook.fr>
2018-08-20 09:32:51 +0200

    component vehicule

--- src/components/RentCar.js
+++ src/components/RentCar.js
@@ -5,8 +5,29 @@ class RentCar extends Component {
   render() {
     return (
       <div className="RentCar">
-        <h1>RentCar</h1>
-        <Vehicule name="Citroen" priceHour="13" priceDay="15"></Vehicule>
+        <h1>RentCar Component</h1>
+        <Vehicule
+          model="C3"
+          priceHour="13"
+          priceDay="15"
+          img="/images/vw-polo-5d-schwarz-2016.png"
+          type="economique"
+          brand="Citroen"
+          nb_doors="5"
+          nb_seets="5"
+        >
+        </Vehicule>
+        <Vehicule
+          model="C3"
+          priceHour="13"
+          priceDay="15"
+          img="/images/vw-polo-5d-schwarz-2016.png"
+          type="economique"
+          brand="Citroen"
+          nb_doors="5"
+          nb_seets="5"
+        >
+        </Vehicule>
       </div>
     );
   }
--- src/components/Vehicule.js
+++ src/components/Vehicule.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Grid, Col, Button} from 'react-bootstrap'
+import '../App.css'
 
 class Vehicule extends Component {
     // constructor(props) {
@@ -7,14 +9,29 @@ class Vehicule extends Component {
     render() {
         return (
             <div className="Vehicule">
-                {this.props.name}
-                <div>
-                    {this.props.priceHour}/h
-                </div>
-                <div>
-                    {this.props.priceDay}/j
-                </div>
-                <button>Reserver</button>
+                <Grid>
+                    <Col xs={12} md={4} lg={4} >
+                        <img src={this.props.img} alt="img_car" />
+                    </Col>
+                    <Col xs={12} md={4} lg={4}>
+                        <div className="vehicule-detail">
+                            <h1>{this.props.model}</h1>
+                            <h4>{this.props.type}</h4>
+                            <h4>{this.props.brand}</h4>
+                            <h4>{this.props.nb_doors} porte</h4>
+                            <h4>{this.props.nb_seets} place</h4>
+                        </div>
+                    </Col>
+                    <Col xs={6} md={4} lg={4} className='price'>
+                        <div className="price-by-hour">
+                            {this.props.priceHour}€ par heure
+                        </div>
+                        <div className="price-by-day">
+                            {this.props.priceDay}€ par jour
+                        </div>
+                        <Button>Reserver</Button>
+                    </Col>
+                </Grid>
             </div>
         );
     }
